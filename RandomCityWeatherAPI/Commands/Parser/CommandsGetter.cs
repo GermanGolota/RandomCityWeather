eb632b54dab40a335b606bfb7cf867c2c9311ba1
Yaroslav Holota <korova2321@gmail.com>
2020-12-30 22:34:48 +0200

    Refactored commands getter

--- RandomCityWeatherAPI/Commands/Parser/CommandsGetter.cs
+++ RandomCityWeatherAPI/Commands/Parser/CommandsGetter.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RandomCityWeatherAPI.Commands
 {
     public class CommandsGetter : ICommandsGetter
     {
+        private readonly WeatherCommand _weatherCommand;
+
+        public CommandsGetter(WeatherCommand weatherCommand)
+        {
+            this._weatherCommand = weatherCommand;
+        }
         public async Task<List<ICommand>> GetCommands()
         {
             var commands =  new List<ICommand>();
-            commands.Add(new TestCommand());
+            commands.Add(_weatherCommand);
             return commands;
         }
     }
