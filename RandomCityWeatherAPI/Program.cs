7cc2e994ab50c65aafd104c6f3f4617171ce78db
Yaroslav Holota <korova2321@gmail.com>
2020-12-30 22:45:16 +0200

    Added client initialization on creation of application

--- RandomCityWeatherAPI/Program.cs
+++ RandomCityWeatherAPI/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
+using Telegram.Bot;
 
 namespace RandomCityWeatherAPI
 {
@@ -14,7 +16,19 @@ namespace RandomCityWeatherAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            InitializeTelegramClient(host);
+
+            host.Run();
+        }
+
+        private static void InitializeTelegramClient(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetService<ITelegramBotClient>();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
