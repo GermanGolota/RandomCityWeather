3c12afba9646d31022ca66173cf367d2974ac33b
Yaroslav Holota <korova2321@gmail.com>
2020-12-31 16:51:48 +0200

    Added migrations to accomodate addition of coordinates

--- RandomCityWeatherAPI/Migrations/20201231144633_CityCoordinates.Designer.cs
+++ RandomCityWeatherAPI/Migrations/20201231144633_CityCoordinates.Designer.cs
@@ -3,14 +3,16 @@ using DataAccessLibrary.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace RandomCityWeatherAPI.Migrations
 {
     [DbContext(typeof(CityContext))]
-    partial class CityContextModelSnapshot : ModelSnapshot
+    [Migration("20201231144633_CityCoordinates")]
+    partial class CityCoordinates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RandomCityWeatherAPI/Migrations/20201231144633_CityCoordinates.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RandomCityWeatherAPI.Migrations
+{
+    public partial class CityCoordinates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "Cities",
+                type: "decimal(12,8)",
+                precision: 12,
+                scale: 8,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "Cities",
+                type: "decimal(12,8)",
+                precision: 12,
+                scale: 8,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Cities");
+        }
+    }
+}
