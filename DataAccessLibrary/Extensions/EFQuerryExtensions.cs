2fe7466871f01d985e464556c6c35058c82f8263
Yaroslav Holota <korova2321@gmail.com>
2021-01-01 13:04:52 +0200

    Added extension to order querry in a random fashion

---
+++ DataAccessLibrary/Extensions/EFQuerryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLibrary.Extensions
+{
+    public static class EFQuerryExtensions
+    {
+        public static IQueryable<T> OrderByRandom<T>(this IQueryable<T> querry)
+        {
+            Random rng = new Random();
+            var skip = (int)(rng.NextDouble() * querry.Count());
+            return querry.Skip(skip);
+        }
+    }
+}
