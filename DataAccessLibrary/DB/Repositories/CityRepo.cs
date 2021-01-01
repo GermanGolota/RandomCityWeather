6e8e6d3c48e6ddb1a5dd2e58964873cba6c33013
Yaroslav Holota <korova2321@gmail.com>
2021-01-01 13:05:20 +0200

    Added repository used to querry cities

---
+++ DataAccessLibrary/DB/Repositories/CityRepo.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLibrary.DB.Entity;
+using DataAccessLibrary.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLibrary.DB.Repositories
+{
+    public class CityRepo : ICityRepo
+    {
+        private readonly CityContext _context;
+
+        public CityRepo(CityContext context)
+        {
+            this._context = context;
+        }
+        public List<City> GetRandomCities(int count)
+        {
+            List<City> cities = _context.Cities.OrderByRandom().Take(count).ToList();
+            return cities;
+        }
+
+        public City GetRandomCity()
+        {
+            City city = _context.Cities.OrderByRandom().Take(1).First();
+            return city;
+        }
+    }
+}
---
+++ DataAccessLibrary/DB/Repositories/ICityRepo.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLibrary.DB.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.DB.Repositories
+{
+    public interface ICityRepo
+    {
+        City GetRandomCity();
+        List<City> GetRandomCities(int count);
+    }
+}
