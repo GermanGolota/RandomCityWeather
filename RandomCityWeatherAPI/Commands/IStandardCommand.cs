77f2a8506d55473d39db5d813108e7bc41af9000
Yaroslav Holota <korova2321@gmail.com>
2021-01-02 08:14:48 +0200

    Created interfaces for standard and non standard commands

---
+++ RandomCityWeatherAPI/Commands/IStandardCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using TelegramBot;
+
+namespace RandomCityWeatherAPI.Commands
+{
+    public interface IStandardCommand : ICommand
+    {
+    }
+}
